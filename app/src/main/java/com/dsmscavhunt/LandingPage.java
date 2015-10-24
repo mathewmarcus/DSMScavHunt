d96fad8bdb0a6ac1bdd0aaba106632f9f7d8eb0a
BlakeMar <blake.martin@drake.edu>
2015-10-24 13:55:39 -0500

    New activity pages, could rename later

--- app/src/main/java/com/dsmscavhunt/LandingPage.java
+++ app/src/main/java/com/dsmscavhunt/LandingPage.java
@@ -5,18 +5,18 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
-public class MainActivity extends AppCompatActivity {
+public class LandingPage extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_list);
+        setContentView(R.layout.activity_landing_page);
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_list, menu);
+        getMenuInflater().inflate(R.menu.menu_landing_page, menu);
         return true;
     }
 
