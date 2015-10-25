cbe61dea2bbf1928276492c342fd7277ce273b91
KatieRoth <katherine.roth@drake.edu>
2015-10-25 16:12:07 -0500

    Added Button to the Landing Page to start the parade

--- app/src/main/java/com/dsmscavhunt/LandingPage.java
+++ app/src/main/java/com/dsmscavhunt/LandingPage.java
@@ -1,9 +1,12 @@
 package com.dsmscavhunt;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 public class LandingPage extends AppCompatActivity {
 
@@ -11,6 +14,18 @@ public class LandingPage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_landing_page);
+
+        //Katie Roth
+        //A button to start the bulldog parade
+        //Sends the user to the first bulldog to find
+        Button startButton = (Button) findViewById(R.id.button_start);
+
+        startButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent intent = new Intent(LandingPage.this,TaskPage.class);
+                startActivity(intent);
+            }
+        });
     }
 
     @Override
