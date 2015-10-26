8e93478bcc0d8d2f919fc1fe54b734a236d8ecaf
BlakeMar <blake.martin@drake.edu>
2015-10-26 09:42:06 -0500

    Renamed activity pages

--- app/src/main/java/com/dsmscavhunt/LandingPage.java
+++ app/src/main/java/com/dsmscavhunt/LandingPage.java
@@ -1,37 +1,22 @@
 package com.dsmscavhunt;
 
-import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.Button;
 
 public class LandingPage extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_landing_page);
-
-        //Katie Roth
-        //A button to start the bulldog parade
-        //Sends the user to the first bulldog to find
-        Button startButton = (Button) findViewById(R.id.button_start);
-
-        startButton.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                Intent intent = new Intent(LandingPage.this,TaskPage.class);
-                startActivity(intent);
-            }
-        });
+        setContentView(R.layout.activity_task_page);
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_landing_page, menu);
+        getMenuInflater().inflate(R.menu.menu_list, menu);
         return true;
     }
 
