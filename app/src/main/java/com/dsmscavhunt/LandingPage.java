6075a2196f80f75d342a9d4b9eb0bae088eda9ec
BlakeMar <blake.martin@drake.edu>
2015-10-26 11:35:39 -0500

    Camera functions added. ImageView soon to be removed

--- app/src/main/java/com/dsmscavhunt/LandingPage.java
+++ app/src/main/java/com/dsmscavhunt/LandingPage.java
@@ -1,16 +1,32 @@
 package com.dsmscavhunt;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 public class LandingPage extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_task_page);
+        setContentView(R.layout.activity_landing_page);
+
+        //Katie Roth
+        //A button to start the bulldog parade
+        //Sends the user to the first bulldog to find
+        Button startButton = (Button) findViewById(R.id.button_start);
+
+        startButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent intent = new Intent(LandingPage.this, TaskPage.class);
+                startActivity(intent);
+            }
+        });
     }
 
     @Override
--- app/src/main/java/com/dsmscavhunt/TaskPage.java
+++ app/src/main/java/com/dsmscavhunt/TaskPage.java
@@ -1,31 +1,64 @@
 package com.dsmscavhunt;
 
 import android.content.Intent;
+import android.graphics.drawable.Drawable;
+import android.net.Uri;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
+
+import java.io.File;
 
 public class TaskPage extends AppCompatActivity {
+
+    Button button;
+    ImageView imageView;
+    static final int CAM_REQUEST = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_landing_page);
-
-        //Katie Roth
-        //A button to start the bulldog parade
-        //Sends the user to the first bulldog to find
-        Button startButton = (Button) findViewById(R.id.button_start);
-
-        startButton.setOnClickListener(new View.OnClickListener() {
+        setContentView(R.layout.activity_task_page);
+        button = (Button) findViewById(R.id.button);
+        imageView = (ImageView) findViewById(R.id.image_button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
             public void onClick(View v) {
-                Intent intent = new Intent(TaskPage.this, LandingPage.class);
-                startActivity(intent);
+                Intent camera_intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                File file = getFile();
+                camera_intent.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(file));
+                startActivityForResult(camera_intent, CAM_REQUEST);
+
             }
         });
+    }
+
+    private File getFile() {
+        File folder = new File("sdcard/camera_app");
+
+        if (!folder.exists()) {
+            folder.mkdir();
+        }
+
+        File image_file = new File(folder, "cam_image1.jpg");
+
+        for(int i=1; image_file.exists(); i++){
+            image_file = new File(folder, "cam_image" + i + ".jpg");
+        }
+
+        return image_file;
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        String path = "sdcard/camera_app/cam_image.jpg";
+
+        imageView.setImageDrawable(Drawable.createFromPath(path));
     }
 
     @Override
