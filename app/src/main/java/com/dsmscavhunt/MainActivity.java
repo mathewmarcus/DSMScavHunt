1b7617423e9b78f283b0e370da9d8a41c429cced
Mathew Marcus <mathewmarcus456@gmail.com>
2015-11-01 16:57:34 -0600

    fixed crashing and included location checker

--- app/src/main/java/com/dsmscavhunt/MainActivity.java
+++ app/src/main/java/com/dsmscavhunt/MainActivity.java
@@ -1,6 +1,10 @@
 package com.dsmscavhunt;
 
+import android.content.Context;
 import android.content.Intent;
+import android.location.Address;
+import android.location.Geocoder;
+import android.location.Location;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
@@ -12,21 +16,34 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.model.LatLng;
 
 import java.io.File;
+import java.io.IOException;
 import java.util.List;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener, GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener  {
 
     public static final String TAG = MainActivity.class.getSimpleName();
+    private Location myLocation = null;
+    private GoogleApiClient mGoogleApiClient;
     Button button;
     ImageView imageView;
     static final int CAM_REQUEST = 1;
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
 
         TextView scavNumber = (TextView)findViewById(R.id.scavNumber);
         TextView scavName = (TextView)findViewById(R.id.scavName);
@@ -41,7 +58,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         dbh.addScavItem(new ScavItem("Meredith", "2805 University Avenue, Des Moines, IA 50311", "right", R.drawable.meredith));
         dbh.addScavItem(new ScavItem("Olmsted (outside)", "2875 University Avenue, Des Moines, IA 50311", "diagonal", R.drawable.olmsted));
         dbh.addScavItem(new ScavItem("Cheeseburger", "1315 30th Street, Des Moines, IA 50311", "straight", R.drawable.cheeseburger));
-        dbh.addScavItem(new ScavItem("West Village One", "1325 31st Street, Des Moines, IA 50311", "crooked", R.drawable.westvillage1));
+        dbh.addScavItem(new ScavItem("West Village One", "1326 31st Street, Des Moines, IA 50311", "crooked", R.drawable.westvillage1));
         dbh.addScavItem(new ScavItem("West Village Two", "1325 31st Street, Des Moines, IA 50311", "sharp left", R.drawable.westvillage2));
         dbh.addScavItem(new ScavItem("Bookstore", "3003 Forest Ave, Des Moines, IA 50311", "sharp right", R.drawable.bulldog1));
         dbh.addScavItem(new ScavItem("Lab Coat (Cline)", "2802 Forest Avenue, Des Moines, IA 50311", "merge right", R.drawable.labcoat));
@@ -88,7 +105,20 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             }
         });
 
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .addApi(LocationServices.API)
+                .build();
+
     }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mGoogleApiClient.connect();
+    }
+
 
     private File getFile()
     {
@@ -143,7 +173,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
             int rowNumbertoDisplay = Integer.parseInt(((String) scavNumber.getText()).trim());
 
-            if (rowNumbertoDisplay >= 9) {
+            if (rowNumbertoDisplay > 9) {
                 rowNumbertoDisplay = 0;
             }
 
@@ -155,8 +185,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             scavImage.setImageResource(scavItems.get(rowNumbertoDisplay).get_image());
 
             rowNumbertoDisplay++;
-        }
-        else if (v.getId() == R.id.mapButton) {
+        } else if (v.getId() == R.id.mapButton) {
             DBHandler dbh = new DBHandler(this);
             int numRows = dbh.getScavItemCount();
             List<ScavItem> scavItems = dbh.getAllScavItems();
@@ -169,8 +198,26 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             Intent i = new Intent(this, MapsActivity.class);
             i.putExtra("address", scavItems.get(rowNumbertoDisplay).get_address());
             startActivity(i);
+        } else if (v.getId() == R.id.checkButton) {
+            DBHandler dbh = new DBHandler(this);
+            int numRows = dbh.getScavItemCount();
+            List<ScavItem> scavItems = dbh.getAllScavItems();
+
+
+            TextView scavNumber = (TextView) findViewById(R.id.scavNumber);
+
+            int rowNumbertoDisplay = Integer.parseInt(((String) scavNumber.getText()).trim());
+
+            LatLng bulldogLatLng = stringToLatLong(scavItems.get(rowNumbertoDisplay).get_address());
+
+            Location bulldogLocation = new Location("test");
+            bulldogLocation.setLatitude(bulldogLatLng.latitude);
+            bulldogLocation.setLongitude(bulldogLatLng.longitude);
+
+            equals(bulldogLocation, myLocation);
         }
     }
+
 
     @Override
     public void onSaveInstanceState(Bundle savedInstanceState) {
@@ -192,6 +239,56 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     protected void onStop() {
         super.onStop();
         Log.i(TAG, "Method: onSaveInstanceState");
+
+    }
+
+    @Override
+    public void onConnected(Bundle bundle) {
+        Log.i(TAG, "Location services connected.");
+        myLocation = LocationServices.FusedLocationApi.getLastLocation(
+                mGoogleApiClient);
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+        Log.i(TAG, "Location services suspended. Please reconnect");
+    }
+
+    public LatLng stringToLatLong(String musicVenue) {
+        Geocoder geo = new Geocoder(this);
+        List<Address> venueAddressList = null;
+        Address venueAddress = null;
+        LatLng venueLatLng = null;
+        try {
+            venueAddressList = geo.getFromLocationName(musicVenue, 1);
+            venueAddress = venueAddressList.get(0);
+            venueLatLng = new LatLng(venueAddress.getLatitude(), venueAddress.getLongitude());
+        } catch (IOException e) {
+            Log.i(TAG, "Failed to convert address to coordinates", e);
+        }
+
+        return venueLatLng;
+
+    }
+
+    public void equals(Location bulldogLocation, Location currentLocation) {
+        float radiusOfError = 100;
+        Context context = getApplicationContext();
+        CharSequence success = "Success!";
+        CharSequence failure = "Incorrect, take another picture and try again.";
+        int duration = Toast.LENGTH_LONG;
+        if (bulldogLocation.distanceTo(currentLocation) <= radiusOfError) {
+            Toast result = Toast.makeText(context, success, duration);
+            result.show();
+        }
+        else {
+            Toast result = Toast.makeText(context, failure, duration);
+            result.show();
+        }
+    }
+
+    @Override
+    public void onConnectionFailed(ConnectionResult connectionResult) {
 
     }
 }
