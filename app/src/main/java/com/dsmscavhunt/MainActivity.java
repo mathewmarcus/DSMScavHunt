c410b766a027650eb8cc2118783e77df00ccf8ff
Mathew Marcus <mathewmarcus456@gmail.com>
2015-11-10 17:46:22 -0600

    location fixes

--- app/src/main/java/com/dsmscavhunt/MainActivity.java
+++ app/src/main/java/com/dsmscavhunt/MainActivity.java
@@ -23,6 +23,8 @@ import android.widget.Toast;
 
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationListener;
+import com.google.android.gms.location.LocationRequest;
 import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.model.LatLng;
 
@@ -30,7 +32,7 @@ import java.io.File;
 import java.io.IOException;
 import java.util.List;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener, GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener  {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener, GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener, LocationListener {
 
     public static final String TAG = MainActivity.class.getSimpleName();
     private Location myLocation = null;
@@ -38,6 +40,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     Button button;
     ImageView imageView;
     static final int CAM_REQUEST = 1;
+    private LocationRequest mLocationRequest;
 
 
 
@@ -60,16 +63,16 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         DBHandler dbh = new DBHandler(this);
 
         // Add items to database
-        dbh.addScavItem(new ScavItem("Aliber", "2847 University Avenue, Des Moines, IA 50311", "Task One: \n This bulldog means business, \n wearing the suit and tie of dogs \n - a simple black collar", R.drawable.aliberdog));
-        dbh.addScavItem(new ScavItem("Meredith", "2805 University Avenue, Des Moines, IA 50311", "Task Two: \n A dog is a man’s best friend. \n You can find this jersey-wearing \n bulldog with Meredith", R.drawable.meredithdog));
-        dbh.addScavItem(new ScavItem("Olmsted (outside)", "2875 University Avenue, Des Moines, IA 50311", "Task Three: \n Drake’s ‘signature’ bulldog lives right \n outside the campus hub for students", R.drawable.olmstedoutsidedog));
-        dbh.addScavItem(new ScavItem("Olmsted (inside)", "2875 University Avenue, Des Moines, IA 50311", "Task Four: \n A picture’s worth a thousand words? \n The bulldog, at home in the epicenter of \n student life, has a least a million things to say", R.drawable.olmstedinsidedog));
+        dbh.addScavItem(new ScavItem("Aliber", "2847 University Avenue, Des Moines, IA 50311", "Task One: \n This bulldog means business, \n wearing the suit and tie of dogs \n - a simple black collar.", R.drawable.aliberdog));
+        dbh.addScavItem(new ScavItem("Meredith", "2805 University Avenue, Des Moines, IA 50311", "Task Two: \n A dog is a man’s best friend. \n You can find this jersey-wearing \n bulldog with Meredith.", R.drawable.meredithdog));
+        dbh.addScavItem(new ScavItem("Olmsted (outside)", "2875 University Avenue, Des Moines, IA 50311", "Task Three: \n Drake’s ‘signature’ bulldog lives right \n outside the campus hub for students.", R.drawable.olmstedoutsidedog));
+        dbh.addScavItem(new ScavItem("Olmsted (inside)", "2875 University Avenue, Des Moines, IA 50311", "Task Four: \n A picture’s worth a thousand words? \n The bulldog, at home in the epicenter of \n student life, has a least a million things to say.", R.drawable.olmstedinsidedog));
         dbh.addScavItem(new ScavItem("Cheeseburger", "1315 30th Street, Des Moines, IA 50311", "Task Five: \n You are what you eat. \n Or at least the cheeseburger bulldog is.", R.drawable.hubbelldog));
         dbh.addScavItem(new ScavItem("West Village One", "1325 31st Street, Des Moines, IA 50311", "Task Six: \n To the west, to the west. \n Every bulldog at the village to the west.", R.drawable.westvillagedog));
-        dbh.addScavItem(new ScavItem("Bookstore", "3003 Forest Ave, Des Moines, IA 50311", "Task Seven: \n Need some cliff notes or textbooks? \n Find this bulldog buying his books", R.drawable.bookstoredog));
+        dbh.addScavItem(new ScavItem("Bookstore", "3003 Forest Ave, Des Moines, IA 50311", "Task Seven: \n Don't judge a bulldog by its cover. \n Or was it books?", R.drawable.bookstoredog));
         dbh.addScavItem(new ScavItem("Lab Coat (Cline)", "2802 Forest Avenue, Des Moines, IA 50311", "Task Eight: \n Bulldogs in lab coats are inCLINEd \n to be more science-savvy than most.", R.drawable.clinedog));
-        dbh.addScavItem(new ScavItem("Stadium", "2719 Forest Avenue", "Task Nine: \n The ultimate Bulldog fan. \n Find this bronze bulldog that \n supports all athletic teams", R.drawable.stadiumdog));
-        dbh.addScavItem(new ScavItem("Basketball", "2601 Forest Avenue, IA 50311", "Task Ten: \n Shivers me timbers. \n This bronze bulldog spends his time \n hanging with the basketball team", R.drawable.basketballdog));
+        dbh.addScavItem(new ScavItem("Stadium", "2719 Forest Avenue", "Task Nine: \n The ultimate Bulldog fan. \n Find this bronze bulldog that \n supports all athletic teams.", R.drawable.stadiumdog));
+        dbh.addScavItem(new ScavItem("Basketball", "2601 Forest Avenue, IA 50311", "Task Ten: \n Shivers me timbers. \n This bronze bulldog spends his time \n hanging with the basketball team.", R.drawable.basketballdog));
 
 
         // Store items in list of ScavItem objects
@@ -101,6 +104,18 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             Log.d("Name: ", log);
         }
 
+        // Register Google API Client to listen for location
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .addApi(LocationServices.API)
+                .build();
+
+        mLocationRequest = LocationRequest.create()
+                .setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY)
+                .setInterval(10 * 1000)        // 10 seconds, in milliseconds
+                .setFastestInterval(1 * 1000); // 1 second, in milliseconds
+
         button = (Button) findViewById(R.id.cam_button);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -112,12 +127,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             }
         });
 
-        // Register Google API Client to listen for location
-        mGoogleApiClient = new GoogleApiClient.Builder(this)
-                .addConnectionCallbacks(this)
-                .addOnConnectionFailedListener(this)
-                .addApi(LocationServices.API)
-                .build();
+
 
     }
 
@@ -257,6 +267,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         Log.i(TAG, "Location services connected.");
         myLocation = LocationServices.FusedLocationApi.getLastLocation(
                 mGoogleApiClient);
+            LocationServices.FusedLocationApi.requestLocationUpdates(mGoogleApiClient, mLocationRequest, this);
     }
 
     @Override
@@ -286,7 +297,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         Radius of error is set to allow the user to be within 100m of target
      */
     public void equals(Location bulldogLocation, Location currentLocation, List<ScavItem> scavItems) {
-        float radiusOfError = 100;
+        float radiusOfError = 300;
         Context context = getApplicationContext();
         CharSequence success = "Success!";
         CharSequence failure = "Incorrect, take another picture and try again.";
@@ -339,5 +350,15 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     @Override
     public void onConnectionFailed(ConnectionResult connectionResult) {
         Log.i(TAG, "Location services unavailable. Please connect");
+    }
+
+    private void handleNewLocation(Location location) {
+        Log.d(TAG, location.toString());
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+        myLocation = location;
+        handleNewLocation(myLocation);
     }
 }
